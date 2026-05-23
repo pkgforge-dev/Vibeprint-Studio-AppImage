9a3df621fbc125d8428b2c176602695791f3ec2e
Link4Electronics <link4electronics@gmail.com>
2026-05-23 19:08:06 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,27 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+    cargo       \
+    cups        \
+    libcups     \
+    lcms2       \
+    libxrandr   \
+    ghostscript \
+    libtiff
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+echo "Building Vibeprint Studio..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/crenedecotret/vibeprint"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone --depth 1 "$REPO" ./vibeprint
+echo "$VERSION" > ~/version
 
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+mkdir -p ./AppDir/bin
+cd vibeprint
+cargo build --release
+mv target/release/vibeprint target/release/studio ../AppDir/bin
