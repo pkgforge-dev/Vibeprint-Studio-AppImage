bc236da68c682d3f831b45d34e233d254d2de868
Link4Electronics <link4electronics@gmail.com>
2026-08-15 06:16:15 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -19,10 +19,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
-
-# If the application needs to be manually built that has to be done down here
 echo "Making nightly build of Vibeprint Studio..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/crenedecotret/vibeprint"
