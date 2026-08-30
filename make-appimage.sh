dbb8c0eeff401d7b062ab1c7c4c251bd843d4968
Link4Electronics <link4electronics@gmail.com>
2026-08-30 06:06:50 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,6 +8,7 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/crenedecotret/vibeprint/refs/heads/main/assets/icons/vibeprint-studio.png
+export USE_HOST_DRIVERS_EXPERIMENTAL=1
 
 # Deploy dependencies
 quick-sharun ./AppDir/bin/studio ./AppDir/bin/vibeprint
